566076abac932db804d19b6a39b633962720a7d8
sanjayvercel <110152498+sanjayvercel@users.noreply.github.com>
2022-07-28 11:39:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Creating a Frontend App
         </a>
       </header>
     </div>
